ffb49c6236ebae28a0952fb0af0b29d5021eeada
Colton Firestone <colton.firestone@gmail.com>
2024-01-09 16:00:31 -0800

    fixed errror in source code for project

--- app/models/Post.py
+++ app/models/Post.py
@@ -4,15 +4,25 @@ from .Vote import Vote
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
 from sqlalchemy.orm import relationship, column_property
 
+from sqlalchemy import select, func
+from sqlalchemy.ext.hybrid import hybrid_property
+
 class Post(Base):
-  __tablename__ = 'posts'
-  id = Column(Integer, primary_key=True)
-  title = Column(String(100), nullable=False)
-  post_url = Column(String(100), nullable=False)
-  user_id = Column(Integer, ForeignKey('users.id'))
-  created_at = Column(DateTime, default=datetime.now)
-  updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
-  user = relationship('User')
-  comments = relationship('Comment', cascade='all,delete')
-  votes = relationship('Vote', cascade='all,delete')
-  vote_count = column_property(lambda: select([func.count(Vote.id)]).where(Vote.post_id == id))
+   __tablename__ = 'posts'
+   id = Column(Integer, primary_key=True)
+   title = Column(String(100), nullable=False)
+   post_url = Column(String(100), nullable=False)
+   user_id = Column(Integer, ForeignKey('users.id'))
+   created_at = Column(DateTime, default=datetime.now)
+   updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+   user = relationship('User')
+   comments = relationship('Comment', cascade='all,delete')
+   votes = relationship('Vote', cascade='all,delete')
+
+   @hybrid_property
+   def vote_count(self):
+       return len(self.votes)
+
+   @vote_count.expression
+   def vote_count(cls):
+       return select([func.count(Vote.id)]).where(Vote.post_id == cls.id).label('vote_count')
